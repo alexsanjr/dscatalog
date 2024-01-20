866734f8afc424d6b8a867c2af10467bd1e97589
Alex Sandro <alex.sandro.1199@gmail.com>
2024-01-20 10:25:37 -0300

    Delete Category

--- backend/src/main/java/com/alexsanjr/dscatalog/controllers/CategoryController.java
+++ backend/src/main/java/com/alexsanjr/dscatalog/controllers/CategoryController.java
@@ -4,6 +4,7 @@ import com.alexsanjr.dscatalog.dto.CategoryDTO;
 import com.alexsanjr.dscatalog.services.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -47,6 +48,12 @@ public class CategoryController {
     public ResponseEntity<CategoryDTO> update(@PathVariable Long id, @RequestBody CategoryDTO dto) {
         dto = service.update(id, dto);
         return ResponseEntity.ok(dto);
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        service.delete(id);
+        return ResponseEntity.noContent().build();
     }
 
 }
--- backend/src/main/java/com/alexsanjr/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/alexsanjr/dscatalog/services/CategoryService.java
@@ -3,10 +3,13 @@ package com.alexsanjr.dscatalog.services;
 import com.alexsanjr.dscatalog.dto.CategoryDTO;
 import com.alexsanjr.dscatalog.entities.Category;
 import com.alexsanjr.dscatalog.repositories.CategoryRepository;
+import com.alexsanjr.dscatalog.services.exceptions.DatabaseException;
 import com.alexsanjr.dscatalog.services.exceptions.ResourceNotFoundException;
 import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
@@ -45,6 +48,19 @@ public class CategoryService {
             return new CategoryDTO(entity);
         } catch (EntityNotFoundException e) {
             throw new ResourceNotFoundException("Id not found " + id);
+        }
+    }
+
+    @Transactional(propagation = Propagation.SUPPORTS)
+    public void delete(Long id) {
+        if(!repository.existsById(id)) {
+            throw new ResourceNotFoundException("Resource not found!");
+        }
+        try {
+            repository.deleteById(id);
+        }
+        catch (DataIntegrityViolationException e) {
+            throw new DatabaseException("Falha de integridade referencial");
         }
     }
 }
