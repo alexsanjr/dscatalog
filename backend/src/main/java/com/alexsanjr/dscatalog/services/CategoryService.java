2495bc17bfec46f40ef70cc3a7ff08e4bc0c2e93
Alex Sandro <alex.sandro.1199@gmail.com>
2024-01-20 10:14:07 -0300

    Put Category

--- backend/src/main/java/com/alexsanjr/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/alexsanjr/dscatalog/services/CategoryService.java
@@ -4,6 +4,7 @@ import com.alexsanjr.dscatalog.dto.CategoryDTO;
 import com.alexsanjr.dscatalog.entities.Category;
 import com.alexsanjr.dscatalog.repositories.CategoryRepository;
 import com.alexsanjr.dscatalog.services.exceptions.ResourceNotFoundException;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -33,5 +34,17 @@ public class CategoryService {
         Category entity = new Category(dto.id(), dto.name());
         entity = repository.save(entity);
         return new CategoryDTO(entity);
+    }
+
+    @Transactional
+    public CategoryDTO update(Long id, CategoryDTO dto) {
+        try {
+            Category entity = repository.getReferenceById(id);
+            entity.setName(dto.name());
+            entity = repository.save(entity);
+            return new CategoryDTO(entity);
+        } catch (EntityNotFoundException e) {
+            throw new ResourceNotFoundException("Id not found " + id);
+        }
     }
 }
