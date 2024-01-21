699e4464fd52da2f540eb9a524b98a9ddc8e89b1
Alex Sandro <alex.sandro.1199@gmail.com>
2024-01-21 15:38:17 -0300

    Fixture BeforeEach

--- backend/src/test/java/com/alexsanjr/dscatalog/repositories/ProductRepositoryTests.java
+++ backend/src/test/java/com/alexsanjr/dscatalog/repositories/ProductRepositoryTests.java
@@ -2,6 +2,7 @@ package com.alexsanjr.dscatalog.repositories;
 
 import com.alexsanjr.dscatalog.entities.Product;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
@@ -14,10 +15,17 @@ public class ProductRepositoryTests {
     @Autowired
     private ProductRepository repository;
 
+    private Long existingId;
+    private Long nonExistingId;
+
+    @BeforeEach
+    void setUp() throws Exception {
+        existingId = 1L;
+        nonExistingId = 100L;
+    }
+
     @Test
     public void deleteShouldDeleteObjectWhenIdExists() {
-        long existingId = 1L;
-
         repository.deleteById(existingId);
         Optional<Product> result = repository.findById(existingId);
 
