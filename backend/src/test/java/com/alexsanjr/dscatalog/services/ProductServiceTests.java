b8ac0d315581b7c8be671c669ddd3dbdaa77b4b8
Alex Sandro <alex.sandro.1199@gmail.com>
2024-01-21 16:32:08 -0300

    ProductServiceTests and mock

---
+++ backend/src/test/java/com/alexsanjr/dscatalog/services/ProductServiceTests.java
@@ -0,0 +1,17 @@
+package com.alexsanjr.dscatalog.services;
+
+import com.alexsanjr.dscatalog.repositories.ProductRepository;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@ExtendWith(SpringExtension.class)
+public class ProductServiceTests {
+
+    @InjectMocks
+    private ProductService service;
+
+    @Mock
+    private ProductRepository repository;
+}
