ec9d885c9002a542fc8b60226ab2beb5c5701c69
Alex Sandro <alex.sandro.1199@gmail.com>
2024-01-23 09:20:12 -0300

    ProductServiceIT findAll

--- backend/src/test/java/com/alexsanjr/dscatalog/services/ProductServiceIT.java
+++ backend/src/test/java/com/alexsanjr/dscatalog/services/ProductServiceIT.java
@@ -1,5 +1,6 @@
 package com.alexsanjr.dscatalog.services;
 
+import com.alexsanjr.dscatalog.dto.ProductDTO;
 import com.alexsanjr.dscatalog.repositories.ProductRepository;
 import com.alexsanjr.dscatalog.services.exceptions.ResourceNotFoundException;
 import org.junit.jupiter.api.Assertions;
@@ -7,8 +8,13 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.transaction.annotation.Transactional;
 
 @SpringBootTest
+@Transactional
 public class ProductServiceIT {
 
     @Autowired
@@ -26,6 +32,35 @@ public class ProductServiceIT {
         existingId = 1L;
         nonExistingId = 1000L;
         countTotalProducts = 25L;
+    }
+
+    @Test
+    public void findAllShouldReturnPageWhenPage0Size10() {
+        PageRequest pageRequest = PageRequest.of(0, 10);
+        Page<ProductDTO> result = service.findAll(pageRequest);
+
+        Assertions.assertFalse(result.isEmpty());
+        Assertions.assertEquals(0, result.getNumber());
+        Assertions.assertEquals(10, result.getSize());
+        Assertions.assertEquals(countTotalProducts, result.getTotalElements());
+    }
+
+    @Test
+    public void findAllShouldReturnEmptyPageWhenPageDoesNotExists() {
+        PageRequest pageRequest = PageRequest.of(50, 10);
+        Page<ProductDTO> result = service.findAll(pageRequest);
+
+        Assertions.assertTrue(result.isEmpty());
+    }
+
+    public void findAllShouldReturnSortedPageWhenSortByName() {
+        PageRequest pageRequest = PageRequest.of(0, 10, Sort.by("name"));
+        Page<ProductDTO> result = service.findAll(pageRequest);
+
+        Assertions.assertTrue(result.isEmpty());
+        Assertions.assertEquals("Macbook Pro", result.getContent().get(0).name());
+        Assertions.assertEquals("Pc Gamer", result.getContent().get(1).name());
+        Assertions.assertEquals("Pc Gamer Alfa", result.getContent().get(2).name());
     }
 
     @Test
