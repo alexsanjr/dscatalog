92dbf7cd81affbf6c01af2e2920a0bc75773884b
Alex Sandro <alex.sandro.1199@gmail.com>
2024-01-23 09:03:21 -0300

    ProductServiceIT delete

---
+++ backend/src/test/java/com/alexsanjr/dscatalog/services/ProductServiceIT.java
@@ -0,0 +1,43 @@
+package com.alexsanjr.dscatalog.services;
+
+import com.alexsanjr.dscatalog.repositories.ProductRepository;
+import com.alexsanjr.dscatalog.services.exceptions.ResourceNotFoundException;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class ProductServiceIT {
+
+    @Autowired
+    private ProductService service;
+
+    @Autowired
+    private ProductRepository repository;
+
+    private Long existingId;
+    private Long nonExistingId;
+    private Long countTotalProducts;
+
+    @BeforeEach
+    void setUp() throws Exception {
+        existingId = 1L;
+        nonExistingId = 1000L;
+        countTotalProducts = 25L;
+    }
+
+    @Test
+    public void deleteShouldDeleteResourceWhenIdExists() {
+        service.delete(existingId);
+
+        Assertions.assertEquals(countTotalProducts - 1, repository.count());
+    }
+
+    public void deleteShouldThrowResourceNotFoundExceptionWhenIdDoesNotExists() {
+        Assertions.assertThrows(ResourceNotFoundException.class,() -> {
+            service.delete(nonExistingId);
+        });
+    }
+}
