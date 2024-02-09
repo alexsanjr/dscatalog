dcbaf4bd21b683be9ac8a16c995a87d7a2010a70
Alex Sandro <alex.sandro.1199@gmail.com>
2024-02-09 17:03:35 -0300

    Send email with token

---
+++ backend/src/main/java/com/alexsanjr/dscatalog/repositories/PasswordRecoverRepository.java
@@ -0,0 +1,8 @@
+package com.alexsanjr.dscatalog.repositories;
+
+import com.alexsanjr.dscatalog.entities.PasswordRecover;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PasswordRecoverRepository extends JpaRepository<PasswordRecover, Long> {
+
+}
--- backend/src/main/java/com/alexsanjr/dscatalog/services/AuthService.java
+++ backend/src/main/java/com/alexsanjr/dscatalog/services/AuthService.java
@@ -1,12 +1,55 @@
 package com.alexsanjr.dscatalog.services;
 
 import com.alexsanjr.dscatalog.dto.EmailDTO;
+import com.alexsanjr.dscatalog.entities.PasswordRecover;
+import com.alexsanjr.dscatalog.entities.User;
+import com.alexsanjr.dscatalog.repositories.PasswordRecoverRepository;
+import com.alexsanjr.dscatalog.repositories.UserRepository;
+import com.alexsanjr.dscatalog.services.exceptions.ResourceNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.Instant;
+import java.util.UUID;
 
 @Service
 public class AuthService {
 
+    @Value(value = "${email.password-recover.token.minutes}")
+    private Long tokenMinute;
+    @Value(value = "${email.password-recover.uri}")
+    private String recoverUri;
+
+    @Autowired
+    private EmailService emailService;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private PasswordRecoverRepository passwordRecoverRepository;
+
+    @Transactional
     public void createRecoverToken(EmailDTO body) {
 
+        User user = userRepository.findByEmail(body.email());
+
+        if (user == null) {
+            throw new ResourceNotFoundException("Email not found");
+        }
+
+        String token = UUID.randomUUID().toString();
+
+        PasswordRecover entity = new PasswordRecover();
+        entity.setEmail(body.email());
+        entity.setToken(token);
+        entity.setExpiration(Instant.now().plusSeconds(tokenMinute * 60));
+        entity = passwordRecoverRepository.save(entity);
+
+        String emailBody = "Click the link to set your new password\n\n"
+                + recoverUri + token +". Validity of " + tokenMinute + " minutes.";
+        emailService.sendEmail(entity.getEmail(), "Password recover", emailBody);
     }
 }
